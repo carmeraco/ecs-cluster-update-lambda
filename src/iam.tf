37016f4f21a6c721d1dd294d154dbb13a6edf9f9
Peter Bell <peter@carmera.co>
2019-02-22 09:36:06 -0800

    fix more copy paste issues

--- src/iam.tf
+++ src/iam.tf
@@ -127,7 +127,7 @@ resource "aws_iam_role_policy_attachment" "roll_lambda_basic_execution" {
 
 ##############################################################
 resource "aws_iam_policy" "roll_lambda_permissions" {
-  name        = "${aws_iam_role.tag_lambda.name}-permissions-${var.region}"
+  name        = "${aws_iam_role.roll_lambda.name}-permissions-${var.region}"
   description = "Mark old ECS instances before rolling cluster update"
   path        = "/"
   policy      = "${data.aws_iam_policy_document.roll_lambda_permissions.json}"
@@ -169,6 +169,6 @@ data "aws_iam_policy_document" "roll_lambda_permissions" {
 }
 
 resource "aws_iam_role_policy_attachment" "roll_lambda_permissions" {
-  role       = "${aws_iam_role.tag_lambda.name}"
+  role       = "${aws_iam_role.roll_lambda.name}"
   policy_arn = "${aws_iam_policy.roll_lambda_permissions.arn}"
 }
